e356bee25130a9a3e348def558f14dc04a6ceb73
elshan22 <elshansaber@gmail.com>
2025-02-17 11:48:10 +0330

    initialize the project

---
+++ src/main/java/com/example/sahab/SahabApplication.java
@@ -0,0 +1,13 @@
+package com.example.sahab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SahabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SahabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/sahab/SahabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sahab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SahabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
