8af331e6d44308d58df67d9964df43866607cf5e
elshan22 <elshansaber@gmail.com>
2025-02-17 19:01:51 +0330

    add microservices

---
+++ file-ingester/src/main/java/com/example/fileingester/FileingesterApplication.java
@@ -0,0 +1,13 @@
+package com.example.fileingester;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileingesterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FileingesterApplication.class, args);
+	}
+
+}
